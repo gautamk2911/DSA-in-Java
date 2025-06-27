09e46fb9483498269a70551526fa91c35c0c989c
Gautam Krishna <119658247+gautamk2911@users.noreply.github.com>
2025-06-27 10:17:54 +0530

    Create FibonaciSeries.java

---
+++ Recursion/FibonaciSeries.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class FibonaciSeries {
+	
+	public static int fibo(int n) {
+		if(n==0) {
+			return 0;
+		}
+		if(n==1) {
+			return 1;
+		}
+		return fibo(n-1) + fibo(n-2);
+	}
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println(fibo(n));
+	}
+}
