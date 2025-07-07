ff164ee6e7efdea6f80af734978debaf57ef595e
Gautam Krishna <119658247+gautamk2911@users.noreply.github.com>
2025-07-07 10:44:24 +0530

    Create InsertAtBottomOfStack.java

---
+++ Problems/InsertAtBottomOfStack.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+class Solution {
+    public Stack<Integer> insertAtBottom(Stack<Integer> st, int x) {
+        if(st.isEmpty()){
+            st.push(x);
+            return st;
+        }
+        int top = st.peek();
+        st.pop();
+        
+        st = insertAtBottom(st,x);
+        st.push(top);
+        
+        
+        
+        return st;
+    }
+}
