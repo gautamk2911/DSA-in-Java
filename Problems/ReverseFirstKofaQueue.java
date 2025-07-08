5a92d33914fbed68b4a3e5bb7327a60753c29a32
Gautam Krishna <119658247+gautamk2911@users.noreply.github.com>
2025-07-08 10:15:07 +0530

    Create ReverseFirstKofaQueue.java

---
+++ Problems/ReverseFirstKofaQueue.java
@@ -0,0 +1,27 @@
+class Solution {
+    public Queue<Integer> reverseFirstK(Queue<Integer> q, int k) {
+        if (q == null || k <= 0 || k > q.size()) {
+            return q;
+        }
+
+        Stack<Integer> st = new Stack<>();
+
+       
+        for (int i = 0; i < k; i++) {
+            st.push(q.poll());
+        }
+
+       
+        while (!st.isEmpty()) {
+            q.add(st.pop());
+        }
+
+       
+        int size = q.size();
+        for (int i = 0; i < size - k; i++) {
+            q.add(q.poll());
+        }
+
+        return q;
+    }
+}
